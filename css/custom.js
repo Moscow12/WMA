59c66dc3677644f1ac545351677f1d11b3305e95
GPITG MESHACK <Moscow12>
2021-02-05 00:21:40 +0300

    first commit hello

---
+++ css/custom.js
@@ -0,0 +1,10 @@
+$('.clearable.example .ui.selection.dropdown')
+  .dropdown({
+    clearable: true
+});
+
+$('.clearable.example .ui.inline.dropdown')
+  .dropdown({
+    clearable: true,
+    placeholder: 'any'
+});
